324b9a2f035177291b607cd873e4bd7c1bb55f68
krmax44 <hi@krmax44.de>
2025-01-13 13:52:12 +0100

    👷 deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import Icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/real-o-mat/',
   output: 'static',
   integrations: [tailwind(), vue()],
   vite: {
