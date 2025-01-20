e55ab9d914d0266bc661277ff79798fae5003348
krmax44 <hi@krmax44.de>
2025-01-20 12:39:45 +0100

    ✨ make party positions collapsible

--- src/components/ResultComments.vue
+++ src/components/ResultComments.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
 import type { Question } from '../content.config'
 import IconBack from '~icons/material-symbols/arrow-back'
 import IconForward from '~icons/material-symbols/arrow-forward'
+import IconChevron from '~icons/material-symbols/keyboard-arrow-down-rounded'
 import { partyNames } from '../store'
 
 const currentQuestionIndex = ref(0)
@@ -33,7 +35,7 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="p-4">
+  <div class="bg-white p-4">
     <h2 class="text-4xl font-medium">Begründung der Thesen</h2>
     <p class="mt-4 mb-6">
       Wie begründen die Parteien ihre Positionen? Wählen Sie eine These aus und
@@ -46,14 +48,14 @@ const props = defineProps<{
       <button
         @click="previousQuestion"
         :disabled="currentQuestionIndex === 0"
-        class="btn-text"
+        class="btn-text justify-self-start"
       >
         <IconBack class="me-1" />
         Zurück
       </button>
       <div class="text-center">
         <select
-          class="rounded-md bg-blue-100 px-4 py-1 border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+          class="rounded-md bg-blue-100 px-4 py-1 w-24 border-gray-300 shadow-sm focus:ring focus:ring-blue-600/50 outline-none motion-safe:transition"
           v-model="currentQuestionIndex"
           @change="transitionName = undefined"
         >
@@ -62,7 +64,7 @@ const props = defineProps<{
             :key="index"
             :value="index"
           >
-            These {{ index + 1 }}
+            These {{ index + 1 }}: {{ question.thesis }}
           </option>
         </select>
       </div>
@@ -76,35 +78,42 @@ const props = defineProps<{
       </button>
     </nav>
 
-    <Transition :name="transitionName" mode="out-in">
-      <article :key="currentQuestionIndex">
-        <h3 class="text-xl md:text-2xl my-8 font-medium">
-          {{ currentQuestion.thesis }}
-        </h3>
-        <ul class="mt-8 grid md:grid-cols-2 gap-4">
-          <li
-            v-for="({ party, comment }, i) in currentQuestion.answers"
-            :key="party"
-            class="flex flex-col"
+    <article>
+      <h3 class="text-xl md:text-2xl my-8 font-medium">
+        {{ currentQuestion.thesis }}
+      </h3>
+      <ul class="mt-8 grid md:grid-cols-2 gap-4">
+        <Disclosure
+          as="li"
+          v-slot="{ open }"
+          v-for="({ party, comment }, i) in currentQuestion.answers"
+          :key="party"
+          class="flex flex-col"
+        >
+          <DisclosureButton
+            class="flex w-full justify-between items-center rounded bg-blue-100 px-4 py-2 focus:ring focus:ring-blue-600/50 outline-none motion-safe:transition"
+            :class="{'rounded-b-none': open}"
           >
-            <div
-              class="flex w-full justify-between items-center rounded-t-lg bg-blue-100 px-4 py-2"
-            >
-              <h4 class="text-lg">{{ partyNames[party] }}</h4>
-            </div>
-            <div class="p-4 bg-blue-50 rounded-b-lg flex-1">
-              <div class="comment" v-if="comment" v-html="comment" />
-              <em v-else>keine Begründung angegeben</em>
-            </div>
-          </li>
-        </ul>
-      </article>
-    </Transition>
+            <h4 class="text-lg">{{ partyNames[party] }}</h4>
+            <IconChevron
+            class="h-5 w-5 text-blue-900 transform motion-safe:transition-transform"
+              :class="{
+                'rotate-180': open
+              }"
+            />
+          </DisclosureButton>
+          <DisclosurePanel class="p-4 bg-blue-50 rounded-b-lg flex-1">
+            <div class="comment" v-if="comment" v-html="comment" />
+            <em v-else>keine Begründung angegeben</em>
+          </DisclosurePanel>
+        </Disclosure>
+      </ul>
+    </article>
   </div>
 </template>
 
 <style scoped>
 .comment:deep(a) {
-  @apply text-blue-600 underline;
+  @apply text-blue-600 hover:text-blue-700 underline;
 }
 </style>
