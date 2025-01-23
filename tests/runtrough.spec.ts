076ef13cbf6e591869c1981c95a482ffbfc9cd21
krmax44 <hi@krmax44.de>
2025-01-23 22:30:25 +0100

    ✅ add e2e tests

---
+++ tests/runtrough.spec.ts
@@ -0,0 +1,69 @@
+import { test, expect } from '@playwright/test'
+
+test('test', async ({ page }) => {
+  test.setTimeout(60_000)
+
+  await page.goto('/')
+  await page.getByRole('button', { name: "Los geht's!" }).click()
+  await page.getByRole('button', { name: 'ja, finde ich auch' }).click()
+  await page.getByRole('button', { name: 'These überspringen' }).click()
+  await expect(page.getByRole('progressbar')).toHaveAttribute(
+    'aria-valuenow',
+    '3',
+  )
+
+  for (let i = 0; i < 18; i++) {
+    await page.waitForTimeout(300)
+    await page.getByRole('button', { name: 'ja, finde ich auch' }).click()
+  }
+
+  await page
+    .locator('label')
+    .filter({ hasText: '1. Arbeit' })
+    .getByLabel('')
+    .check()
+  await page
+    .locator('label')
+    .filter({ hasText: '19. Landwirtschaft' })
+    .getByLabel('')
+    .check()
+
+  await expect(page.locator('form input[type="checkbox"]')).toHaveCount(19)
+
+  await page.getByRole('button', { name: 'Weiter' }).click()
+
+  await expect(page.getByLabel('Ergebnis').getByRole('heading')).toContainText(
+    'Ihr Real-O-Mat Ergebnis',
+  )
+
+  await page.getByRole('tab', { name: 'Parteienvergleich' }).click()
+  await expect(
+    page.getByLabel('Parteienvergleich').getByRole('heading'),
+  ).toContainText('Vergleich')
+
+  await expect(
+    page.locator('tr:nth-child(1) > td:nth-child(2) div[aria-label]'),
+  ).toHaveAttribute('aria-label', 'ja, finde ich auch')
+  await expect(
+    page.locator('tr:nth-child(2) > td:nth-child(2) div[aria-label]'),
+  ).toHaveAttribute('aria-label', 'keine Antwort vorhanden')
+
+  await page.getByRole('tab', { name: 'Begründungen' }).click()
+  await expect(page.getByLabel('Begründungen').locator('h2')).toContainText(
+    'Begründung der Thesen',
+  )
+  await expect(page.getByLabel('Springe zur These')).toHaveValue('0')
+
+  await page.getByRole('button', { name: 'Weiter' }).click()
+  await expect(page.getByLabel('Springe zur These')).toHaveValue('1')
+
+  await page.getByRole('link', { name: 'Real-O-Mat' }).click()
+  await expect(page.getByRole('button', { name: "Los geht's!" })).toBeVisible()
+
+  await page.getByRole('button', { name: "Los geht's!" }).click()
+  await expect(page.getByRole('article')).toContainText('20 / 20')
+
+  page.on('dialog', (dialog) => dialog.accept())
+  await page.getByRole('button', { name: 'Neustarten' }).click()
+  await expect(page.getByRole('button', { name: "Los geht's!" })).toBeVisible()
+})
