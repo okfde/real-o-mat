d93ce26ba447cb7f3cb6c74d2e310ee17bdc1e37
krmax44 <hi@krmax44.de>
2025-01-27 16:00:11 +0100

    ✨ ask if the user wants to start over when starting again

--- src/pages/_index.vue
+++ src/pages/_index.vue
@@ -1,5 +1,12 @@
 <script lang="ts" setup>
 import { computed, onMounted, ref, watch } from 'vue'
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from '@headlessui/vue'
 import Questionnaire from '../views/Questionnaire.vue'
 import Results from '../views/Results.vue'
 import Weights from '../views/Weights.vue'
@@ -8,17 +15,21 @@ import { useStore, Stage } from '../store'
 import IconBack from '~icons/material-symbols/arrow-back'
 import IconForward from '~icons/material-symbols/arrow-forward'
 import IconRestart from '~icons/material-symbols/restart-alt-rounded'
+import IconClose from '~icons/material-symbols/close-rounded'
 import FdsLogo from '../assets/fragdenstaat.svg'
 import Tutorial from '../views/Tutorial.vue'
 
-defineProps<{
+const props = defineProps<{
   questions: Question[]
 }>()
 
 const { answers, currentQuestionIndex, currentStage, viewTransition } =
   useStore()
 
+const restartDialog = ref(false)
+
 const reset = () => {
+  restartDialog.value = false
   viewTransition.value = 'slide-back'
   answers.value = {}
   currentQuestionIndex.value = 0
@@ -33,6 +44,29 @@ const confirmReset = () => {
 
 const hasPreviousStage = computed(() => currentStage.value !== Stage.Intro)
 const hasNextStage = computed(() => currentStage.value !== Stage.Results)
+
+const start = () => {
+  if (Object.keys(answers.value).length !== 0) {
+    restartDialog.value = true
+  } else {
+    nextStage()
+  }
+}
+
+const toLeftOff = () => {
+  restartDialog.value = false
+
+  if (Object.keys(answers.value).length === props.questions.length) {
+    currentStage.value = Stage.Results
+  } else {
+    currentStage.value = Stage.Questionnaire
+  }
+}
+
+const startOver = () => {
+  reset()
+  nextStage()
+}
 
 const nextStage = () => {
   viewTransition.value = 'slide'
@@ -76,7 +110,7 @@ onMounted(() => updateBeforeUnload())
           Der Real-O-Mat ist keine Wahlempfehlung, sondern ein
           Informationsangebot über Parteien und ihr Abstimmungsverhalten.
         </p>
-        <button class="btn btn-lg start-button mt-4" @click="nextStage">
+        <button class="btn btn-lg start-button mt-4" @click="start">
           <IconForward aria-hidden="true" class="me-1" />
           Los geht's!
         </button>
@@ -132,4 +166,63 @@ onMounted(() => updateBeforeUnload())
       Neustarten
     </button>
   </div>
+
+  <TransitionRoot appear :show="restartDialog" as="template">
+    <Dialog as="div" @close="restartDialog = false" class="relative z-10">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black/25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-lg transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <div class="flex">
+                <DialogTitle as="h3" class="text-lg"> Los geht's! </DialogTitle>
+
+                <button @click="restartDialog = false" class="btn-text ms-auto">
+                  <IconClose aria-label="Schließen" />
+                </button>
+              </div>
+              <div class="mt-2">
+                <p>
+                  Sie haben den Real-O-Mat bereits begonnen. Möchten Sie dort
+                  weitermachen, wo Sie bisher aufgehört haben?
+                </p>
+              </div>
+
+              <div class="mt-4 flex flex-wrap gap-4">
+                <button type="button" class="btn" @click="toLeftOff">
+                  Ja, weitermachen
+                </button>
+                <button type="button" class="btn-outline" @click="startOver">
+                  Nein, von vorne beginnen
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
 </template>
