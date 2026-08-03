990156605b721555e28a8f765c82a7c49163f13a
flooguu <flooguu@users.noreply.github.com>
2026-08-03 14:25:53 +0200

    💄 small UI fixes

--- src/components/ElectionSelection.vue
+++ src/components/ElectionSelection.vue
@@ -20,30 +20,18 @@ const showArchive = ref(false)
 
 <template>
   <div class="mt-8 flex flex-col items-center gap-4">
-    <a
-      v-for="election in activeElections"
-      :key="election.slug"
-      class="btn btn-lg"
-      :href="`/wahlen/${election.slug}`"
-    >
-      <IconForward aria-hidden="true" class="me-1" />
+    <a v-for="election in activeElections" :key="election.slug" class="btn btn-lg" :href="`/wahlen/${election.slug}`">
+      <IconForward aria-hidden="true" class="me-1 shrink-0 max-md:mr-4" />
       {{ election.title }}
     </a>
 
-    <button
-      v-if="archivedElections.length"
-      class="btn-text mt-4"
-      :aria-expanded="showArchive"
-      @click="showArchive = !showArchive"
-    >
+    <button v-if="archivedElections.length" class="btn-text mt-4" :aria-expanded="showArchive"
+      @click="showArchive = !showArchive">
       <IconArchive aria-hidden="true" class="me-1" />
       Archiv
     </button>
 
-    <ul
-      v-if="showArchive"
-      class="flex flex-col items-center gap-3"
-    >
+    <ul v-if="showArchive" class="flex flex-col items-center gap-3">
       <li v-for="election in archivedElections" :key="election.slug">
         <a class="btn-outline" :href="`/wahlen/${election.slug}`">
           {{ election.title }}
--- src/components/ResultShare.vue
+++ src/components/ResultShare.vue
@@ -3,6 +3,7 @@ import { computed } from 'vue'
 import { useClipboard } from '@vueuse/core'
 import { getShareResultText } from '../share'
 import IconShare from '~icons/material-symbols/ios-share-rounded'
+import { publicUrl } from '../const'
 
 const props = defineProps<{
   partyMatches: { party: string; percentage: number }[]
@@ -11,10 +12,24 @@ const props = defineProps<{
 const shareResultText = computed(() => getShareResultText(props.partyMatches))
 
 const { copy, copied } = useClipboard({ copiedDuring: 2000 })
+
+const shareResult = () => {
+  copy(shareResultText.value + `\n\n${publicUrl}`)
+
+  const shareData = {
+    title: shareResultText.value,
+    url: publicUrl,
+  }
+
+  if (navigator.canShare?.(shareData)) {
+    navigator.share(shareData)
+  }
+}
+
 </script>
 
 <template>
-  <button class="btn" @click="copy(shareResultText)">
+  <button class="btn" @click="shareResult">
     <IconShare aria-hidden="true" class="me-1" />
     {{ copied ? 'Kopiert!' : 'Ihr Ergebnis teilen' }}
   </button>
