dc9a387dceb50dea2390418a924d8c9ea2858559
krmax44 <hi@krmax44.de>
2026-08-11 16:36:00 +0200

    💄 polish election selection, add separate archive page

--- src/components/ElectionSelection.vue
+++ src/components/ElectionSelection.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
 import { computed, ref } from 'vue'
 import type { Election } from '../content.config'
-import IconForward from '~icons/material-symbols/arrow-forward'
 import IconArchive from '~icons/material-symbols/archive-outline-rounded'
+import ElectionCard from './ElectionCard.vue'
 
 const props = defineProps<{
   elections: Election[]
@@ -19,24 +19,16 @@ const showArchive = ref(false)
 </script>
 
 <template>
-  <div class="mt-8 flex flex-col items-center gap-4">
-    <a v-for="election in activeElections" :key="election.slug" class="btn btn-lg" :href="`/wahlen/${election.slug}`">
-      <IconForward aria-hidden="true" class="me-1 shrink-0 max-md:mr-4" />
-      {{ election.title }}
-    </a>
-
-    <button v-if="archivedElections.length" class="btn-text mt-4" :aria-expanded="showArchive"
-      @click="showArchive = !showArchive">
-      <IconArchive aria-hidden="true" class="me-1" />
-      Archiv
-    </button>
-
-    <ul v-if="showArchive" class="flex flex-col items-center gap-3">
-      <li v-for="election in archivedElections" :key="election.slug">
-        <a class="btn-outline" :href="`/wahlen/${election.slug}`">
-          {{ election.title }}
-        </a>
+  <nav class="mt-8">
+    <ul class="grid gap-4 md:grid-cols-2">
+      <li v-for="election in activeElections" :key="election.slug">
+        <ElectionCard :election="election" />
       </li>
     </ul>
-  </div>
+
+    <a v-if="archivedElections.length" class="btn-text mt-4" href="/archiv/">
+      <IconArchive aria-hidden="true" class="me-1" />
+      Archiv
+    </a>
+  </nav>
 </template>
