5134b59e23eab0eae5f3b646931cbf76e686dc9a
flooguu <flooguu@users.noreply.github.com>
2026-08-12 11:29:15 +0200

    fix alignment of items in header

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -24,7 +24,7 @@ onMounted(() => {
 
 <template>
   <header
-    class="z-10 container mx-auto grid grid-cols-[1fr_auto] items-center gap-y-1 px-4 py-8 md:py-12 xl:items-start xl:py-24"
+    class="z-10 container mx-auto grid grid-cols-[1fr_auto] items-start gap-y-1 px-4 py-8 md:py-12 xl:items-start xl:py-24"
     @keydown.esc="closeMenu">
     <div>
       <a href="/">
