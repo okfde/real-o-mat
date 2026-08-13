9b03bb56a3840acf76a657689dac1129ee0c6587
flooguu <flooguu@users.noreply.github.com>
2026-08-13 11:34:20 +0200

    allow different election names in share text

--- src/const.ts
+++ src/const.ts
@@ -1,7 +1,7 @@
 export const publicUrl = 'https://real-o-mat.de'
 export const shareText =
   'Reality-Check statt Wahlversprechen. Positionier’ Dich zu politischen Forderungen und finde raus: Mit welcher Wahl wird’s auch real?'
-export const shareResultTitle = 'Ich hab den Real-O-Mat zur Landtagswahl in Sachsen-Anhalt gemacht. Das ist mein Ergebnis:'
+export const shareResultTitle = 'Ich hab den Real-O-Mat zur {electionName} gemacht. Das ist mein Ergebnis:'
 export const shareResultBody = 'Was kommt bei Dir Raus? Probier\'s aus, auf real-o-mat.de'
 export const shareResultEmojiFilled = '🟪'
 export const shareResultEmojiEmpty = '⬜'
